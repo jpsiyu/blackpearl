354a40cb05e0f2cd756d2dd17017e72ec6042042
wusiyu <stephenwu2019@163.com>
2019-12-25 10:04:36 +0800

    lint

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,21 +1,21 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import routes from 'vue-auto-routing'
-import { createRouterLayout } from 'vue-router-layout'
+import Vue from 'vue';
+import Router from 'vue-router';
+import routes from 'vue-auto-routing';
+import { createRouterLayout } from 'vue-router-layout';
 
-Vue.use(Router)
+Vue.use(Router);
 
-const RouterLayout = createRouterLayout(layout => {
-  return import('@/layouts/' + layout + '.vue')
-})
+const RouterLayout = createRouterLayout((layout) => {
+  return import('@/layouts/' + layout + '.vue');
+});
 
 export default new Router({
-  mode: "history",
+  mode: 'history',
   routes: [
     {
       path: '/',
       component: RouterLayout,
-      children: routes
-    }
-  ]
-})
+      children: routes,
+    },
+  ],
+});
