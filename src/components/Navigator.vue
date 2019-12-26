92d99e4ea6e7183c274038c3f99bf5c5fa782ccd
wusiyu <stephenwu2019@163.com>
2019-12-26 16:39:52 +0800

    select menu

--- src/components/Navigator.vue
+++ src/components/Navigator.vue
@@ -1,34 +1,66 @@
 <template>
   <div class="nav">
-    <div class="nav-item" v-for="(item, index) in projects" :key="index" @click="handleClick(item)">
+    <div
+      class="nav-item"
+      :class="{'selected': selectedId === item.id }"
+      v-for="(item, index) in projects"
+      :key="index"
+      @click="handleClick(item)"
+    >
       <span>{{item.name}}</span>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
+import Vue from "vue";
 export default Vue.extend({
   data() {
-    return {};
+    return {
+      selectedId: 0
+    };
   },
   computed: {
     projects() {
       return [
-        { id: 0, name: '首页', link: '/' },
-        { id: 1, name: '钱包', link: '/wallet' },
-        { id: 2, name: 'DEX', link: '/dex' },
-        { id: 3, name: '聊天', link: '/chat' },
-        { id: 4, name: '房子', link: '/house' },
-        { id: 5, name: '知识', link: '/knowledge' },
+        { id: 0, name: "首页", link: "/" },
+        { id: 1, name: "钱包", link: "/wallet" },
+        { id: 2, name: "DEX", link: "/dex" },
+        { id: 3, name: "聊天", link: "/chat" },
+        { id: 4, name: "房子", link: "/house" },
+        { id: 5, name: "知识", link: "/knowledge" }
       ];
-    },
+    }
+  },
+  watch: {
+    $route: function(val) {
+      this.calSelected(val.path);
+    }
+  },
+  created() {
+    this.calSelected(this.$route.path);
   },
   methods: {
     handleClick(item: any) {
       this.$router.push({ path: item.link });
     },
-  },
+
+    calSelected(path: string) {
+      let target;
+      for (let i = 0; i < this.projects.length; i++) {
+        const proj = this.projects[i];
+        if (proj.link === path) {
+          target = proj;
+          break;
+        }
+      }
+      if (!target) {
+        this.selectedId = -1;
+      } else {
+        this.selectedId = target.id;
+      }
+    }
+  }
 });
 </script>
 
@@ -58,5 +90,10 @@ export default Vue.extend({
     border-radius: 50%;
     box-shadow: 0 0 3px 2px rgba(0, 0, 0, 0.1);
   }
+}
+
+.selected {
+  background: var(--color-success);
+  color: #fff;
 }
 </style>
