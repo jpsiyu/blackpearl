dffb0705e15c7a6b016208d690f4ad80e116627d
wusiyu <stephenwu2019@163.com>
2019-12-26 16:13:32 +0800

    postcss

--- src/components/knowledge/Article.vue
+++ src/components/knowledge/Article.vue
@@ -11,7 +11,7 @@ export default Vue.extend({
   props: ["source"],
   components: {
     "vue-markdown": VueMarkdown
-  },
+  }
 });
 </script>
 
@@ -19,50 +19,49 @@ export default Vue.extend({
 .article {
   padding: 10px;
   margin-bottom: 20px;
-}
 
-.article h1 {
-  margin: 30px 0;
-}
+  h1 {
+    margin: 30px 0;
+  }
 
-.article h2 {
-  margin: 20px 0 10px 0;
-}
+  h2 {
+    margin: 20px 0 10px 0;
+  }
 
-.article h3 {
-  margin: 20px 0 10px 0;
-}
+  h3 {
+    margin: 20px 0 10px 0;
+  }
 
-.article p {
-  line-height: 25px;
-  margin: 20px 0;
-}
+  p {
+    line-height: 25px;
+    margin: 20px 0;
+  }
 
-.article a {
-  text-decoration: none;
-}
+  a {
+    text-decoration: none;
+    &:hover {
+      text-decoration: underline;
+    }
+  }
 
-.article a:hover {
-  text-decoration: underline;
-}
+  img {
+    max-width: 80%;
+    margin: 30px;
+    box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.3);
+  }
 
-.article img {
-  max-width: 80%;
-  margin: 30px;
-  box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.3);
-}
+  ul {
+    line-height: 25px;
+    background-color: rgb(250, 250, 250);
+    color: gray;
+    box-sizing: border-box;
+    padding-top: 20px;
+    padding-bottom: 20px;
+  }
 
-.article ul {
-  line-height: 25px;
-  background-color: rgb(250, 250, 250);
-  color: gray;
-  box-sizing: border-box;
-  padding-top: 20px;
-  padding-bottom: 20px;
-}
-
-.article blockquote {
-  margin-left: 20px;
+  blockquote {
+    margin-left: 20px;
+  }
 }
 </style>
 
