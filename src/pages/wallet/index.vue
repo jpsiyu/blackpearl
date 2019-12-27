ef3da451db889976013a445390016cc5a11bca3f
wusiyu <stephenwu2019@163.com>
2019-12-27 16:38:52 +0800

    ui

--- src/pages/wallet/index.vue
+++ src/pages/wallet/index.vue
@@ -1,6 +1,25 @@
 <template>
   <div class="wallet">
-    <div class="wallet-title">钱包</div>
+    <div class="wallet-top">
+      <div class="wallet-top-account">
+        <span class="wallet-top-account__name">账号1</span>
+        <span class="wallet-top-account__addr">{{
+          currentAcc.checksumAddress
+        }}</span>
+        <span class="wallet-top-account__copy">复制</span>
+      </div>
+      <div class="wallet-top-net">
+        <span>Main Net</span>
+      </div>
+    </div>
+    <div class="wallet-main">
+      <div class="wallet-main-left">
+        <div class="wallet-token" v-for="i in 100" :key="i">钱包</div>
+      </div>
+      <div class="wallet-main-right">
+        haha
+      </div>
+    </div>
   </div>
 </template>
 
@@ -13,13 +32,15 @@ import { BIP32Node } from "@/scripts/wallet/bip32Node";
 interface IData {
   master: BIP32Node | null;
   accounts: BIP32Node[];
+  currentAcc: BIP32Node | null;
 }
 
 export default Vue.extend({
   data(): IData {
     return {
       master: null,
-      accounts: []
+      accounts: [],
+      currentAcc: null
     };
   },
   created() {
@@ -32,6 +53,63 @@ export default Vue.extend({
 
     this.master = new BIP32Node(node);
     this.accounts = [this.master];
+    this.currentAcc = this.accounts[0];
   }
 });
 </script>
+
+<style lang="postcss" scoped>
+.wallet {
+  margin: auto;
+  width: 800px;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  &-top {
+    padding: 0 10px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    height: 50px;
+    &-account {
+      display: flex;
+      align-items: center;
+      height: 100%;
+      span {
+        margin-right: 10px;
+      }
+      &__copy {
+        border: 1px solid var(--color-light-border);
+        padding: 0 10px;
+        border-radius: 20px;
+        cursor: pointer;
+      }
+    }
+  }
+  &-main {
+    flex: 1;
+    border-top: 1px solid var(--color-light-border);
+    display: flex;
+    overflow: hidden;
+    &-left {
+      height: 100%;
+      overflow-y: auto;
+      width: 200px;
+      border-right: 1px solid var(--color-light-border);
+    }
+    &-right {
+      padding: 0 10px;
+      height: 100%;
+      overflow-y: auto;
+      flex: 1;
+    }
+  }
+  &-token {
+    cursor: pointer;
+    padding: 5px 20px;
+    &:hover {
+      background: var(--color-extra-light-border);
+    }
+  }
+}
+</style>
