e80cb212d21bc7e06ce53e663578b919a4eaea29
wusiyu <stephenwu2019@163.com>
2019-12-28 13:10:31 +0800

    footer button

--- src/components/wallet/Setting.vue
+++ src/components/wallet/Setting.vue
@@ -2,7 +2,7 @@
   <el-dialog class="set" :visible.sync="visible" :append-to-body="true">
     <div slot="title">设置</div>
     <el-form>
-      <el-form-item label="切换网络">
+      <el-form-item label="切换网络:">
         <el-select v-model="form.netID">
           <el-option
             v-for="item in networks"
@@ -13,6 +13,10 @@
         </el-select>
       </el-form-item>
     </el-form>
+    <div slot="footer">
+      <el-button @click="hide">取消</el-button>
+      <el-button @click="sure" type="primary">确定</el-button>
+    </div>
   </el-dialog>
 </template>
 
@@ -53,7 +57,8 @@ export default Vue.extend({
     },
     hide() {
       this.visible = false;
-    }
+    },
+    sure() {}
   }
 });
 </script>
