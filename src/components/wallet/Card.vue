aebc1d4e3bff293f569e5aa9dd2d63778e581f93
wusiyu <stephenwu2019@163.com>
2020-01-01 16:27:36 +0800

    add link

--- src/components/wallet/Card.vue
+++ src/components/wallet/Card.vue
@@ -1,8 +1,54 @@
 <template>
-  <div class="card">card</div>
+  <div class="card">
+    <span>余额: {{ balance }}</span>
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-export default Vue.extend({});
+import { mapState } from "vuex";
+
+interface IData {
+  balance: string;
+}
+
+export default Vue.extend({
+  data(): IData {
+    return {
+      balance: "1.23"
+    };
+  },
+  computed: {
+    ...mapState({
+      currentAcc: (state: any) => state.wallet.currentAcc,
+      currentNet: (state: any) => state.wallet.currentNet,
+      currentCoin: (state: any) => state.wallet.currentCoin
+    })
+  },
+  watch: {
+    currentAcc: function() {
+      this.handleChange();
+    },
+    currentCoin: function() {
+      this.handleChange();
+    },
+    currentNet: function() {
+      this.handleChange();
+    }
+  },
+  created() {
+    this.handleChange();
+  },
+  methods: {
+    handleChange() {
+      console.log("something change!");
+    }
+  }
+});
 </script>
+
+<style lang="postcss" scoped>
+.card {
+  padding: 10px;
+}
+</style>
