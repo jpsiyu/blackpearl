1c40ff8905938aafc9b96e641db54db19d4ef3d8
wusiyu <stephenwu2019@163.com>
2020-01-06 12:00:09 +0800

    add element declare file

--- src/plugins/i18n.ts
+++ src/plugins/i18n.ts
@@ -1,5 +1,7 @@
 import Vue from "vue";
 import VueI18n, { LocaleMessages } from "vue-i18n";
+import enLocale from 'element-ui/lib/locale/lang/en'
+import zhLocale from 'element-ui/lib/locale/lang/zh-CN'
 
 Vue.use(VueI18n);
 
@@ -14,10 +16,26 @@ function loadLocaleMessages(): LocaleMessages {
     const matched = key.match(/([A-Za-z0-9-_]+)\./i);
     if (matched && matched.length > 1) {
       const locale = matched[1];
-      messages[locale] = locales(key);
+      const msg = locales(key)
+
+      // add element support
+      const msgWithElemnt = addElementLocale(msg, locale)
+      messages[locale] = msgWithElemnt
     }
   });
   return messages;
+}
+
+function addElementLocale(origin:any, locale: string) {
+  if (locale === 'en') {
+    return Object.assign(origin, enLocale)
+  }
+
+  if (locale === 'zh-cn') {
+    return Object.assign(origin, zhLocale)
+  }
+
+  return origin
 }
 
 export default new VueI18n({
